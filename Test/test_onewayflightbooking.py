96e41c56b7b139e9183d82c681aa1669e9c09f93
mim2021 <jannatulferdousm7@gmail.com>
2022-11-17 17:22:04 +0600

    Update Project

--- Test/test_onewayflightbooking.py
+++ Test/test_onewayflightbooking.py
@@ -19,7 +19,6 @@ class TestOneWayFlightBooking(unittest.TestCase):
         login_oneway_flight_booking.enter_email_login("mim@sharetrip.net")
         login_oneway_flight_booking.enter_password_login("Vugijugi78")
         login_oneway_flight_booking.click_login_button()
-        time.sleep(2)
 
         oneway_flight = OneWayFlight(driver)
         oneway_flight.click_oneway_tab()
@@ -28,31 +27,25 @@ class TestOneWayFlightBooking(unittest.TestCase):
 
         oneway_flight = DateForFlights(driver)
         oneway_flight.select_target_date("January 2023", 21)
-        time.sleep(4)
 
         oneway_flight = OneWayFlight(driver)
         oneway_flight.click_economy()
         oneway_flight.click_search_button()
-        time.sleep(2)
         oneway_flight.click_cross_button()
-        time.sleep(1)
         oneway_flight.click_book_button()
-        time.sleep(4)
         oneway_flight.switch_window()
         print(driver.title)
         oneway_flight.click_title()
         oneway_flight.input_given_name(''.join(random.choices(string.ascii_letters, k=10)))
         oneway_flight.input_sur_name(''.join(random.choices(string.ascii_letters, k=8)))
-        time.sleep(4)
         # oneway_flight.click_radio_button_earn_tc()
         oneway_flight.click_radio_button_redeem_tc()
-        time.sleep(4)
-
         driver.execute_script("window.scrollTo(0, 600)")
         oneway_flight.input_phone_number(f"{random.randrange(100000000000)}")
         oneway_flight.select_DOB()
         postcode = f"{random.randrange(10000)}"
-        # postcode = random.randrange(1000,9999) """another way to generate dynamic postcode"""
+        """another way to generate dynamic postcode"""
+        # postcode = random.randrange(1000,9999)
         oneway_flight.input_postcode(postcode)
 
         def get_random_passport_number(length=1):
@@ -69,7 +62,7 @@ class TestOneWayFlightBooking(unittest.TestCase):
         oneway_flight.click_tc_checkbox()
         # oneway_flight.click_paynow_button()
         time.sleep(4)
-        # print("done successfully")
+        print("Test Case Run Successfully")
 
 
 
